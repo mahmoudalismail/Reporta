9eb905311558818e60fbd3959c8441da28520116
Lingliang Zhang <lingliangz@gmail.com>
2014-11-29 09:57:35 -0500

    more unit tests written

--- server/server.py
+++ server/server.py
@@ -6,11 +6,11 @@ class EchoHandler(tornado.web.RequestHandler):
     def get(self):
         self.write(tornado.escape.json_encode({
             "status": 200,
-            "data": "Hello World success"
+            "data": "Hello World"
         }))
 
     def post(self):
-        data = tornado.escape.json_encode(self.request.body)
+        data = tornado.escape.json_decode(self.request.body)
         self.write(tornado.escape.json_encode({
             "status": 200,
             "data": data
--- server/tests.py
+++ server/tests.py
@@ -1,4 +1,5 @@
 import tornado.testing
+import tornado.httputil
 import server
 import unittest
 
@@ -6,12 +7,27 @@ class EchoTests(tornado.testing.AsyncHTTPTestCase):
     def get_app(self):
         return server.get_app()
 
-    def test_homepage(self):
-        self.assertEqual(True, True)
+    @tornado.testing.gen_test
+    def get(self):
+        response = yield self.http_client.fetch(self.get_url("/echo"))
+        payload = tornado.escape.json_decode(response.body)
+        self.assertEqual(payload["status"], 200)
+        self.assertEqual(payload["data"], "Hello World")
+
+    @tornado.testing.gen_test
+    def post(self):
+        data = {"test": "result"}
+        response = yield self.http_client.fetch(self.get_url("/echo"),
+                                                method="POST",
+                                                headers=tornado.httputil.HTTPHeaders({"content-type": "application/json"}),
+                                                body=tornado.escape.json_encode(data))
+        payload = tornado.escape.json_decode(response.body)
+        self.assertEqual(payload["data"], data)
 
 def all():
     suite = unittest.TestSuite()
-    suite.addTest(EchoTests("test_homepage"))
+    suite.addTest(EchoTests("get"))
+    suite.addTest(EchoTests("post"))
     return suite
 
 def main():
